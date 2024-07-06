9e016c506d60acacf439a7a0f37ededf6b23a603
Rashidmerejan <alikhanurfbandali@gmail.com>
2024-07-06 16:30:07 +0500

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ global.email ="saimsamsun789@gmail.com"
 global.location="Lahore,Pakistan."
 
 
-global.mongodb= process.env.MONGODB_URI || ""
+global.mongodb= process.env.MONGODB_URI || "mongodb+srv://dadl:Iamdadl123@atlascluster.yv7hl0a.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster"
 global.allowJids= process.env.ALLOW_JID || "null" 
 global.blockJids= process.env.BLOCK_JID || "null"
 global.DATABASE_URL = process.env.DATABASE_URL || ""
